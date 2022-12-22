6aaf831098a82dd285e7d9161a7d7e3400008fe2
Sanya <a.kondratev9@gmail.com>
2022-12-22 21:25:34 +0300

    episod 2

--- Program.cs
+++ Program.cs
@@ -10,3 +10,28 @@ for (int i = 0; i < array.Length; i++)
     string txt = Console.ReadLine();
     array[i] = txt;
 }
+SortingArray(array, size);
+string [] array2 = SortingArray(array, size);
+
+
+string [] SortingArray(string [] arr1, int s) 
+{
+    string [] arr2 = new string[s];  // создаем массив и включаем в него строки не более 3 символов
+    int j = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length < 4)
+        {
+            arr2[j] = arr1[i];
+            j++;
+        }
+    }
+    string [] arr3 = new string [j];  // создаем 3 массив по размеру соответсвующему количеству найденных элементов
+    int l = 0;
+    for (int k = 0; k < j; k++)
+    {
+        arr3[l] = arr2[k];
+        l++;
+    }
+    return arr3;
+}
